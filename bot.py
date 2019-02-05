5c986294d71cb74d2558abea870136feb619cd8f
Astr0l <41875791+Astr0l@users.noreply.github.com>
2019-02-05 17:35:35 -0600

    Function to load cogs when launched

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import json
 
 
-TOKEN = ""
+TOKEN = "NTQyNDg2Nzk5NDkxMTM3NTUw.Dzuttw.FzKIIAP6Xg_clgnWw7Q95KEbcRM"
 
 cogs = []
 
@@ -22,7 +22,17 @@ __________________________________
 [!] Status: Online!
 [/] Signed in as: {bot.user} 
 [/] Servers Connected: {len(bot.guilds)}
+[~] Initializing cogs...
 """
 	print(msg)
+
+if __name__ == "__main__":
+	for cog in cogs:
+		try:
+			bot.load_extension(cog)
+			print("[*] Successfully loaded {cog}")
+		except Exception as e:
+			print(f"[*] Unable to load {cog}: {e}")
+	print("[~] Finished loading cogs!\n[/] Switched to monitoring mode...have a nice day!")
 
 bot.run(TOKEN)
