136d96dea0780ac371548836b11b95e2f3f4e013
Astr0l <41875791+Astr0l@users.noreply.github.com>
2019-02-06 21:34:07 -0600

    Added syntax highlighting for help

--- cogs/bundle_core.py
+++ cogs/bundle_core.py
@@ -14,14 +14,14 @@ class Core:
     @commands.command()
     async def reload(self, ctx, cog:str=None):
         if cog == None or cog == " ":
-            return await ctx.send("Invalid cog name!\nUsage: ./reload cogs.<cog name>")
+            return await ctx.send("Invalid cog name!\n```\nUsage: ./reload cogs.<cog name>\n```")
         try:
             await ctx.send("Reloading extension...")
             self.bot.unload_extension(cog)
             self.bot.load_extension(cog)
             await ctx.send("Successfully loaded extension!")
         except Exception as e:
-            return await ctx.send(f"Failed to reload extension: {e}")
+            return await ctx.send(f"Failed to reload extension:\n```\n{e}\n```")
         
 def setup(bot):
     bot.add_cog(Core(bot))
