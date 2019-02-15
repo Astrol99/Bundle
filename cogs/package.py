5d3bc07f44a4358815dab154392617bdaae90dbc
Astr0l <41875791+Astr0l@users.noreply.github.com>
2019-02-14 23:33:37 -0600

    Added install feature (uncompleted)

--- cogs/package.py
+++ cogs/package.py
@@ -1,9 +1,29 @@
 import discord
 from discord.ext import commands
+import os
+from os import listdir
+from os.path import isfile, join
+import subprocess
+import sys
 
 class Package:
     def __init__(self, bot):
         self.bot = bot
+
+    
+    @commands.command()
+    async def install(self, ctx, url:str=None):
+        if url == "" or url == " " or url == None:
+            return await ctx.send("Invalid url!")
+        try:
+            await ctx.send(f"Starting to install cog...")
+            os.system(f"cd cogs && git clone {url}")
+            await ctx.send("Cloned repository cog...")
+            #await ctx.send("Trying to find cog file...")
+            #os.system(f"cd cog")
+        except Exception as e:
+            return await ctx.send(f"Error installing cog...please contact the developer for help! Details: ```{e}```")
+        await ctx.send("Successfully installed cog!")
 
 def setup(bot):
     bot.add_cog(Package(bot))
